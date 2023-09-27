45f6b0b0ca83f2d445dc4af02fdbeec287e94d57
Marcos Casas <85005420+marcos700x@users.noreply.github.com>
2023-09-27 10:34:38 -0600

    Codigo de la animacion del navbar

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,15 @@ var splide = new Splide( '.banner', {
 
   splide.mount(  );
 
+//Animacion en scroll para el navbar
+var LastScroll = document.documentElement.scrollTop || document.body.scrollTop;
+window.onscroll = function (){
+  var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if(LastScroll < scroll){
+    document.querySelector("nav").style.transform = "translateY(-100%)"
+  }else{
+    document.querySelector("nav").style.transform = "translateY(0)"
+  }
+  LastScroll = scroll
+}
 
